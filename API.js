9c2dafc0eb0154fcb87e0725595ba703fdd59ff5
vQuang <duyanh2732@gmail.com>
2023-11-14 10:31:44 +0700

    quang

---
+++ API.js
@@ -0,0 +1,7 @@
+export const URL = 'http://192.168.1.252:8000/';
+export const API_GETCATEGORY = URL + 'app-list-category';
+export const API_HOT_PRODUCTS = URL + 'app-list-hot-order-product';
+export const API_HOTSALE_PRODUCTS = URL +'app-list-sale-order-product';
+export const API_NEW_PRODUCTS = URL +'app-list-new-order-product';
+export const API_PRODUCTS = URL + '';
+export const API_CART = URL + '';
